16d0955a1a93d64b27d61f97aa5ed1fd29298454
Lautaro-Vel <lauvelmor305@gmail.com>
2025-11-25 22:00:24 -0300

    correccion

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,7 @@
 import React, { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import AppSimplificado from './AppSimplificado.jsx'
-import AuthContextProvider from './contextos/authContext.jsx'
-import FeedContextProvider from './contextos/feedContext.jsx'
-import DetailsContextProvider from './contextos/detailsContext.jsx'
-import GroupContextProvider from './contextos/readingGroupContext.jsx'
-import MessageContextProvider from './contextos/messageContext.jsx'
-import UserContextProvider from './contextos/userContext.jsx'
 import { BrowserRouter } from 'react-router'
 
-// Usar app simplificada para debugging
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App/>  
